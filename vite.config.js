13f2d32aadab3ccc73a40dcc686db0afa68f4571
hrdsant <haroldsan9.9@gmail.com>
2023-02-28 09:49:19 -0600

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/parcial-programacion-IV/",
+  base: "/parcialDeployPrueba/",
   plugins: [vue()],
   resolve: {
     alias: {
